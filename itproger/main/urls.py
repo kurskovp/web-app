16780c71bea83b85263e73d3efecf058fb3ef0ad
kurskovp <kurskovp@gmail.com>
2021-06-17 11:43:06 +0300

    Добавлена страница пр Курсы

--- itproger/main/urls.py
+++ itproger/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index)
-    path('about', views.about())
+    path('', views.index),
+    path('about', views.about()),
+    path('course', views.course())
 ]
--- itproger/main/views.py
+++ itproger/main/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Проверка связи')
+    return HttpResponse('<h3>Проверка связи</h4>')
+
 
 def about(request):
-    return HttpResponse("Страница по нас")
+    return HttpResponse('<h3>Страница про нас</h3><p>Наши последнии новости</p>')
+
+def course(request):
+    return HttpResponse('<h3>Страница курсов</h3><p>Это страница курсов!</p>')
