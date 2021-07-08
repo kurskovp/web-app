798e421fe85866266b94227e4075ee1668f89f78
kurskovp <kurskovp@gmail.com>
2021-07-08 13:20:29 +0300

    В приложении main создали html шаблон страницы layout.html

--- itproger/main/views.py
+++ itproger/main/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse('<h3>Это главная страница</h4>')
+    return render(request, 'main/index.html')
 
 
 def about(request):
-    return HttpResponse('<h3>Страница про нас</h3><p>Наши последнии новости</p>')
+    return render(request, 'main/about.html')
 
 
-def course(request):
-    return HttpResponse('<h3>Страница курсов</h3><p>Это страница курсов!</p>')
--- itproger/pictures_app/views.py
+++ itproger/pictures_app/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'pictures_app/index.html')
