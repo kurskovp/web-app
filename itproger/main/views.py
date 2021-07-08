e03737ba937a055443dbaf61a1ebeb4666688207
kurskovp <kurskovp@gmail.com>
2021-07-08 10:01:47 +0300

    Добавлено новое приложение home

--- itproger/main/views.py
+++ itproger/main/views.py
@@ -3,11 +3,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h3>Проверка связи</h4>')
+    return HttpResponse('<h3>Это главная страница</h4>')
 
 
 def about(request):
     return HttpResponse('<h3>Страница про нас</h3><p>Наши последнии новости</p>')
+
 
 def course(request):
     return HttpResponse('<h3>Страница курсов</h3><p>Это страница курсов!</p>')
