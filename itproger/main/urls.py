a3d03d33f43f0f6b9e8866b19ca84cfe0f448745
kurskovp <kurskovp@gmail.com>
2021-09-09 14:13:09 +0300

    Ошибка в сроке { % endblock % }

--- itproger/main/urls.py
+++ itproger/main/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('about', views.about, name = 'about'),
-    path('course', views.course, name = 'course')
+    path('about', views.about, name = 'about')
 ]
